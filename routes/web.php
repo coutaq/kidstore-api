9316cdf5192e2769e404358cb8ea4a3066929831
G-Mell <jlmelnichenko@gmail.com>
2022-04-28 16:12:54 +0300

    блейды и роуты админки

--- routes/web.php
+++ routes/web.php
@@ -27,14 +27,59 @@ Route::post('/sanctum/token', function (Request $request) {
         'password' => 'required',
         'device_name' => 'required',
     ]);
- 
+
     $user = User::where('email', $request->email)->first();
- 
-    if (! $user || ! Hash::check($request->password, $user->password)) {
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
         throw ValidationException::withMessages([
             'email' => ['The provided credentials are incorrect.'],
         ]);
     }
- 
+
     return $user->createToken($request->device_name)->plainTextToken;
+});
+Auth::routes();
+
+Route::get('/categories', function () {
+    return view('categories.index');
+});
+Route::get('/categories/{id}', function ($id) {
+    return view('categories.show', compact('id'));
+});
+
+Route::get('/brands', function () {
+    return view('brands.index');
+});
+Route::get('/brands/{id}', function ($id) {
+    return view('brands.show', compact('id'));
+});
+Route::get('/products/{id}', function ($id) {
+    return view('products.show', compact('id'));
+});
+Route::put('profile/edit', function (Request $request) {
+    $user = Auth::user();
+    $user->update($request->all());
+    return redirect()->route('profile');
+})->name('user.edit');
+
+Route::get('/profile', function () {
+    return view('profile');
+})->name('profile');
+
+Route::get('/orders', function () {
+    return view('orders');
+});
+Route::get('/basket', function () {
+    return view('basket');
+});
+Route::get('/admin', function () {
+    return redirect()->route('category.index');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::resource('category', App\Http\Controllers\AdminCategoryController::class);
+    Route::resource('product', App\Http\Controllers\AdminProductController::class);
+    Route::resource('subcategory', App\Http\Controllers\AdminSubcategoryController::class);
+    Route::resource('brand',  App\Http\Controllers\AdminBrandController::class);
+    Route::resource('order', App\Http\Controllers\AdminSubcategoryController::class);
 });
